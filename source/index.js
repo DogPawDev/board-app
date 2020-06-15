2cf2b9d7388309f1a382d5a9c8d7a1e9af5dc5ec
dogpaw <foodev@naver.com>
2020-06-16 02:01:31 +0900

    bulma 사용 및 UI 기초 작업

--- source/index.js
+++ source/index.js
@@ -2,4 +2,5 @@
 import hello_word from "./hello.js";
 import world_word from "./world.js";
 
-document.querySelector('#root').innerHTML = hello_word +' '+world_word;
+//document.querySelector('#root').innerHTML = hello_word +' '+world_word;
+
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
         path:path.resolve(__dirname,"public"),
         filename:'index_bundle.js'
     }
+    ,
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
 ,
     devServer: {
         host : '127.0.0.1',
